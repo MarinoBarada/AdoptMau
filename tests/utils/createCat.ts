1c8e39d3627d005990244e825bc7a222b797db83
Marino Barada <marino18barada@gmail.com>
2023-11-10 12:17:20 +0100

    Changing and creating for future use

---
+++ tests/utils/createCat.ts
@@ -0,0 +1,10 @@
+import type { Cat } from "@/api/types";
+
+export const createCat = (cat: Partial<Cat> = {}): Cat => ({
+  id: 1,
+  name: "Fluffy",
+  age: 3,
+  color: "gray",
+  picture: "https://cat-image",
+  ...cat,
+})
