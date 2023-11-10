d436455863d9030ae9a8da6eb3747456d670de70
Marino Barada <marino18barada@gmail.com>
2023-11-10 12:16:19 +0100

    Creating cats store with state and action fetch

---
+++ src/stores/cats.ts
@@ -0,0 +1,19 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+
+import getCats from "@/api/getCats";
+import type { Cat } from "@/api/types";
+
+export const useCatsStore = defineStore("cats", () => {
+  const cats = ref<Cat[]>([]);
+
+  const FETCH_CATS = async () => {
+    const receiveCats = await getCats();
+    cats.value = receiveCats;
+  };
+
+  return {
+    cats,
+    FETCH_CATS
+  }
+});
