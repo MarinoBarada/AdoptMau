b018e9171ad1751477577019100f59f440180644
Marino Barada <marino18barada@gmail.com>
2023-11-24 10:54:29 +0100

    Unit test for CatSideBarRadioBox component

---
+++ tests/unit/components/CatResults/CatSideBar/CatSideBarRadioBox.test.ts
@@ -0,0 +1,85 @@
+import type { Mock } from "vitest";
+import { render, screen } from "@testing-library/vue";
+import userEvent from "@testing-library/user-event";
+import { createTestingPinia } from "@pinia/testing";
+
+import CatSideBarRadioBox from "@/components/CatResults/CatSideBar/CatSideBarRadioBox.vue";
+import { useUserStore } from "@/stores/user";
+import type { SortBy } from "@/api/types";
+
+describe("CatSideBarRadioBox", () => {
+  interface CatSideBarRadioBoxProps {
+    sorts: Array<SortBy>;
+    name: String;
+    action: Mock;
+  };
+
+  const createProps = (props: Partial<CatSideBarRadioBoxProps> = {}
+  ): CatSideBarRadioBoxProps => ({
+    sorts: [
+      { name: "Age", value: true },
+      { name: "Name", value: false },
+    ],
+    name: "sortBy",
+    action: vi.fn(),
+    ...props
+  });
+
+  const renderCatSideBarRadioBox = (
+    props: CatSideBarRadioBoxProps
+  ) => {
+    const pinia = createTestingPinia({ stubActions: false });
+    const userStore = useUserStore();
+
+    render(CatSideBarRadioBox, {
+      props: {
+        ...props
+      },
+      global: {
+        plugins: [pinia]
+      }
+    });
+
+    return { userStore };
+  };
+
+  it("renders unique list of values", () => {
+    const props = createProps({
+      sorts: [
+        { name: "Age", value: true },
+        { name: "Name", value: false },
+      ]
+    });
+    renderCatSideBarRadioBox(props);
+
+    const jobTypesListItems = screen.getAllByRole("listitem");
+    const jobTypes = jobTypesListItems.map((node) => {
+      if (node.textContent != null)
+        return node.textContent.trim()
+    });
+
+    expect(jobTypes).toEqual(["Age", "Name"]);
+  });
+
+  describe("when user click radio box", () => {
+    it("communicate that user has selected radio box", async () => {
+      const action = vi.fn();
+      const props = createProps({
+        sorts: [
+          { name: "Ascending", value: true },
+          { name: "Descending", value: false },
+        ],
+        action
+      });
+      renderCatSideBarRadioBox(props);
+
+      const nameRadioBox = screen.getByRole("radio", {
+        name: /descending/i
+      });
+
+      await userEvent.click(nameRadioBox);
+
+      expect(action).toHaveBeenCalledWith("Descending");
+    });
+  })
+});
