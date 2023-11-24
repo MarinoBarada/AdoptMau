906f925d7a8bc53caf9ad509564ec7522f9ad1cd
Marino Barada <marino18barada@gmail.com>
2023-11-24 10:21:32 +0100

    Unit test for CatSideBarSearchName component

---
+++ tests/unit/components/CatResults/CatSideBar/CatSideBarSearchName.test.ts
@@ -0,0 +1,48 @@
+import { render, screen } from "@testing-library/vue";
+import userEvent from "@testing-library/user-event";
+import { createTestingPinia } from "@pinia/testing";
+
+import { useUserStore } from "@/stores/user";
+import CatSideBarSearchNames from "@/components/CatResults/CatSideBar/CatSideBarSearchNames.vue";
+
+describe("CatSideBarSearchNames", () => {
+  const renderCatSideBarSearchNames = () => {
+    const pinia = createTestingPinia();
+    const userStore = useUserStore();
+
+    render(CatSideBarSearchNames, {
+      global: {
+        plugins: [pinia]
+      }
+    });
+
+    return { userStore };
+  };
+
+  it("populates search input from store", async () => {
+    const { userStore } = renderCatSideBarSearchNames();
+    userStore.nameSearch = "Oreo";
+    const input = await screen.findByRole<HTMLInputElement>("textbox");
+    expect(input.value).toBe("Oreo");
+  });
+
+  it("writes user input to store", async () => {
+    const { userStore } = renderCatSideBarSearchNames();
+    userStore.nameSearch = "";
+    const input = screen.getByRole<HTMLInputElement>("textbox");
+    await userEvent.type(input, "O");
+    await userEvent.click(document.body);
+
+    expect(userStore.UPDATE_NAME_SEARCH).toHaveBeenCalledWith("O");
+  });
+
+  it("removes whitespace from user input", async () => {
+    const { userStore } = renderCatSideBarSearchNames();
+    userStore.nameSearch = "";
+    const input = screen.getByRole<HTMLInputElement>("textbox");
+    await userEvent.type(input, "  Oreo   ");
+    await userEvent.click(document.body);
+
+    expect(userStore.UPDATE_NAME_SEARCH).toHaveBeenCalledWith("Oreo");
+  });
+});
